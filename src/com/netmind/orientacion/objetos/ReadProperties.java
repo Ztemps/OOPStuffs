b0e8b53eab45bc085e453f6bf9dd7744667a8e19
AULES\A5Alumno <javicasados@gmail.com>
2016-09-14 13:48:02 +0200

    TryCatch with resources

--- src/com/netmind/orientacion/objetos/ReadProperties.java
+++ src/com/netmind/orientacion/objetos/ReadProperties.java
@@ -1,25 +1,23 @@
 package com.netmind.orientacion.objetos;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class ReadProperties {
 
-	public static String getDatosProperties(String campo) throws FileNotFoundException, IOException {
+	public static String getDatosProperties(String campo) throws IOException {
 
 		Properties propiedades = new Properties();
-		InputStream entrada = null;
 		
-		try {
-			entrada = new FileInputStream("configuracion.properties");
+		try (InputStream entrada = new FileInputStream("configuracion.properties")){
 			propiedades.load(entrada);
 			propiedades.getProperty(campo);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw e;		
 		}
 
 		return propiedades.toString();
