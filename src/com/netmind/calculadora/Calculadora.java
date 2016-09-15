54dc86c8848970749b24da032def0c1e766b65be
AULES\A5Alumno <javicasados@gmail.com>
2016-09-15 13:54:55 +0200

    Calculadora Reflexión 2

--- src/com/netmind/calculadora/Calculadora.java
+++ src/com/netmind/calculadora/Calculadora.java
@@ -2,15 +2,41 @@ package com.netmind.calculadora;
 
 public class Calculadora {
 
+	private int num1;
+	private int num2;
+	
+	public Calculadora(){
+		
+	}
+	
+	
+	public Calculadora(int num1,int num2){
+		setNum1(num1);
+		setNum2(num2);
+	}
+	
 	public int suma(int num1, int num2){
 	
 		return num1 + num2;
+	}
+	
+	
+	public int suma(){
+	
+		return getNum1()+getNum2();
+		
 	}
 	
 	public int resta(int num1, int num2){
 		
 		return num1 - num2;
 	}
+	
+	public int resta(){
+		
+		return getNum1()-getNum2();
+	}
+	
 	
 	public int multiplicacion(int num1, int num2){
 		
@@ -22,4 +48,29 @@ public class Calculadora {
 		return num1 / num2;
 	}
 	
+	
+	
+	
+	
+	public int getNum1() {
+		return num1;
+	}
+
+
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+
+
+	public int getNum2() {
+		return num2;
+	}
+
+
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
+
+
+
 }
--- src/com/netmind/calculadora/reflection/CalculadoraReflection.java
+++ src/com/netmind/calculadora/reflection/CalculadoraReflection.java
@@ -2,56 +2,41 @@ package com.netmind.calculadora.reflection;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.NoSuchElementException;
 
 import com.netmind.calculadora.Calculadora;
 
 public class CalculadoraReflection {
 
-	public static void main(String[] args) throws NoSuchMethodException,SecurityException ,
-	IllegalAccessException , IllegalArgumentException , InvocationTargetException{
-		
-		Calculadora objetoCalculadora = new Calculadora();
-		Class objetoInfoDeMiClase = null;
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException {
+
 		try {
-			objetoInfoDeMiClase = Class.forName("com.netmind.calculadora.Calculadora");
-		} catch (ClassNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		Method metodoGetSuma =null;
-		Method metodoGetResta =null;
-		Method metodoGetMultiplicacion=null;
-		Method metodoGetDivision=null;
-		
-		int valorRetornoSuma = 0;
-		int valorRetornoResta = 0;
-		int valorRetornoMultiplicacion = 0;
-		int valorRetornoDivision = 0;
-		
-		try {
-			 metodoGetSuma = objetoInfoDeMiClase.getMethod("suma", int.class, int.class);
-			 metodoGetResta = objetoInfoDeMiClase.getMethod("resta", int.class, int.class);
-			 metodoGetMultiplicacion = objetoInfoDeMiClase.getMethod("multiplicacion", int.class, int.class);
-			 metodoGetDivision = objetoInfoDeMiClase.getMethod("division", int.class, int.class);
 			
-			 valorRetornoSuma = (int) metodoGetSuma.invoke(objetoCalculadora, 10, 5);
-			 valorRetornoResta = (int) metodoGetResta.invoke(objetoCalculadora, 10, 5);
-			 valorRetornoMultiplicacion = (int) metodoGetMultiplicacion.invoke(objetoCalculadora, 10, 5);
-			 valorRetornoDivision = (int) metodoGetDivision.invoke(objetoCalculadora, 10, 5);
-			
+			Calculadora objetoCalculadora = new Calculadora();
+
+			Class objetoInfoDeMiClase = Class.forName("com.netmind.calculadora.Calculadora");
+
+			Method metodoGetSuma = objetoInfoDeMiClase.getMethod("suma", int.class, int.class);
+			Method metodoGetResta = objetoInfoDeMiClase.getMethod("resta", int.class, int.class);
+			Method metodoGetMultiplicacion = objetoInfoDeMiClase.getMethod("multiplicacion", int.class, int.class);
+			Method metodoGetDivision = objetoInfoDeMiClase.getMethod("division", int.class, int.class);
+
+			int valorRetornoSuma = (int) metodoGetSuma.invoke(objetoCalculadora, 10, 5);
+			int valorRetornoResta = (int) metodoGetResta.invoke(objetoCalculadora, 10, 5);
+			int valorRetornoMultiplicacion = (int) metodoGetMultiplicacion.invoke(objetoCalculadora, 10, 5);
+			int valorRetornoDivision = (int) metodoGetDivision.invoke(objetoCalculadora, 10, 5);
+
 			System.out.println("Suma: " + valorRetornoSuma);
 			System.out.println("Resta: " + valorRetornoResta);
 			System.out.println("Mutliplicacion: " + valorRetornoMultiplicacion);
 			System.out.println("Division: " + valorRetornoDivision);
-			
-		} catch (NoSuchMethodException | SecurityException | IllegalAccessException 
+
+		} catch (ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalAccessException
 				| IllegalArgumentException | InvocationTargetException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
-	
+
 }
